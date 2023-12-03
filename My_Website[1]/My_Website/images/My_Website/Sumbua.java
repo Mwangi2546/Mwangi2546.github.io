070831d3755e9021a0b1ee63621e915f95c1f73a
Mwangi2546 <152792350+Mwangi2546@users.noreply.github.com>
2023-12-03 17:20:39 +0300

    Add files via upload

---
+++ My_Website[1]/My_Website/images/My_Website/Sumbua.java
@@ -0,0 +1,15 @@
+public class Sumbua{
+    public static void main(String s[])
+    {
+        int i = 0;
+
+        do{
+            
+            if(i%2==0)
+            System.out.print("i = " + i);
+            i++;
+            
+        }while(i<10);
+       
+    }}
+
