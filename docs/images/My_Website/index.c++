7d3c0aa44849777aea9e8f8d57140a60e6912b70
Mwangi2546 <152792350+Mwangi2546@users.noreply.github.com>
2023-12-04 07:18:05 +0300

    Add files via upload

---
+++ docs/images/My_Website/index.c++
@@ -0,0 +1,14 @@
+#include<stdio>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter the number:";
+    cin>>num;
+    if(num%2==0){
+        cout<<"The number is even";
+    }else{
+        cout<<"The number is odd";
+    }
+
+
+}
---
+++ docs/images/My_Website/projects.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.0/css/fontawesome.min.css" integrity="sha384-z4tVnCr80ZcL0iufVdGQSUzNvJsKjEtqYZjiQrrYKlpGow+btDHDfQWkFjoaz/Zr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+    <script src="https://kit.fontawesome.com/b04f0c3d24.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="css/styless.css">
+</head>
+   
+	<title>KEVOMANIX</title>
+</head>
+<body>
+  <!-- Preloader -->
+<div id="preloader"></div>
+  <nav class="navbar navbar-expand-lg  navbar-right">
+  <a class="navbar-brand" href="#">KEVOMANIX</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav ms-auto ">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="projects.php">Projects</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="solution.php">Error Solutions</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact.php">Contact Us</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<div class="container py-4">
+    <div class="row">
+        <div class="col-md-6 mx-auto">
+            <div class="input-group">
+                <input class="form-control border-end-0 border rounded-pill" type="search" value="Search for an error" id="example-search-input">
+                <span class="input-group-append">
+                    <button class=" ml-2 btn btn-outline-secondary"id="search-btn" type="button">
+                        <i class="fa fa-search" id="search"></i>
+                    </button>
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+                <!--projects-->
+   <div class="container" id="projects">
+              <div class="row gy-5">
+                  <div class="col-md-6">
+                      <div class="cards py-3">
+                        <div class="main-text2">
+                              <h2>How to make a boostrap form using HTML and CSS</h2>
+                              <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                          </div>
+                          <img class="cardsimg" src="images/banner5.avif">
+                          <div><button class="btn btn-primary btn-3">DOWNLOAD SOURCE CODE</button></div>
+                      </div>
+                  </div>
+                  <div class="col-md-6">
+                      <div class="cards py-3">
+                        <div class="main-text2">
+                              <h2>How to make a boostrap form using HTML and CSS</h2>
+                              <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                          </div>
+                          <img class="cardsimg" src="images/banner5.avif">
+                         <div><button class="btn btn-primary btn-3">DOWNLOAD SOURCE CODE</button></div>
+                      </div>
+                  </div>
+                  <br>
+                  <div class="col-md-6">
+                      <div class="cards py-3">
+                        <div class="main-text2">
+                              <h2>How to make a boostrap form using HTML and CSS</h2>
+                              <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                          </div>
+                          <img class="cardsimg" src="images/banner5.avif">
+                         <div><button class="btn btn-primary btn-3">DOWNLOAD SOURCE CODE</button></div>
+                      </div>
+                  </div>
+
+                  <div class="col-md-6">
+                      <div class="cards py-3">
+                        <div class="main-text2">
+                              <h2>How to make a boostrap form using HTML and CSS</h2>
+                              <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                          </div>
+                          <img class="cardsimg" src="images/banner5.avif">
+                         <div><button class="btn btn-primary btn-3">DOWNLOAD SOURCE CODE</button></div>
+                      </div>
+                  </div>
+
+
+              </div>
+          </div>
+ <nav>
+  <ul class="pagination justify-content-center">
+    <li class="page-item disabled">
+      <span class="page-link">Previous</span>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item active" aria-current="page">
+      <span class="page-link">2</span>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#">Next</a>
+    </li>
+  </ul>
+</nav>
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <!--footer-->
+<footer class="bg-dark text-white pt-5 pb-4 site-footer">
+    <div class="container text-center text-md-left">
+        <div class="row text-center text-md-left">
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-warning">KEVOMANIX_CODE</h5>
+                <p>This is the best site to order a website for your organization.We design dynamic website by use of HTML,CSS,Javascript and PHP.Also you can join us and become a member of this great community.</p>
+           <hr class="mb-4 btn-danger">
+            <div class="row mx-auto mt-3">
+                <ul class="socials">
+                <li><i class="fa fa-instagram"></i></li>
+                <li><i class="fa fa-facebook"></i></li>
+                <li><i class="fa fa-twitter"></i></li>
+                <li><i class="fa fa-whatsapp"></i></li>
+                </ul>
+            
+            </div>
+            </div>
+
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Products</h5>
+            <p><a href="#" class="text-white" style="text-decoration: none;">Dynamic websites</a></p>
+            <p><a href="#" class="text-white" style="text-decoration: none;">Job opportunities</a></p>
+            <p><a href="#" class="text-white" style="text-decoration: none;">SourceFiles</a></p>
+            <p><a href="#" class="text-white" style="text-decoration: none;">Mentorship</a></p>
+            </div>
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Useful links</h5>
+            <p><a href="#" class="text-white" style="text-decoration: none;">Home</a></p>
+            <p><a href="#" class="text-white" style="text-decoration: none;">Order a website</a></p>
+            <p><a href="#" class="text-white" style="text-decoration: none;">Join us</a></p>
+            <p><a href="#" class="text-white" style="text-decoration: none;">Help</a></p>
+            </div>
+
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-warning" >Contact</h5>
+                <p><i class="fas fa-home mr-3"></i>Kenya, NY 2333,Nakuru</p>
+                <p><i class="fas fa-envelope mr-3"></i>kelvinmwaniki62@gmail.com</p>
+                <p><i class="fas fa-phone mr-3"></i>+254 719161593</p>
+                <p><i class="fas fa-print mr-3"></i>+01 335 633 77</p>
+            </div>
+        </div>
+ <div class="row align-items-center">
+     <h6 class="modal-footer">&copyCopyright 2022||Designed by KEVO_MANIX</h6>
+ </div>
+        
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+<script>
+    var loader = document.getElementById("preloader");
+    window.addEventListener("load",function(){
+        loader.style.display = "none";
+    })
+</script>
+</body>
+</html>
